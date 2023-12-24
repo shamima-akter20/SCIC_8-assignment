87dad47863c40f74738c6082ea7158a98943ad00
shamima-akter <shamimamirpur2@gmail.com>
2023-12-25 03:07:17 +0600

    login register page update

--- src/Components/Pages.jsx/Login/Login.jsx
+++ src/Components/Pages.jsx/Login/Login.jsx
@@ -1,11 +1,67 @@
-
+import Lottie from "lottie-react";
+import { Link } from "react-router-dom";
+import login from "./login.json";
 
 const Login = () => {
-    return (
-        <div>
-            login
-        </div>
-    );
+  return (
+    <div className="mx-2  md:mx-8 lg:mx-16">
+
+        <h1 className="text-3xl md:text-5xl font-bold text-center my-8">Login </h1>
+
+
+     {/* ------------------------------------ */}
+     {/*              design of Form          */}
+     {/* ------------------------------------ */}
+     <div className="flex justify-center items-center flex-col md:flex-row 
+      md:gap-10 lg:gap-36">
+
+     <div className="flex-1">
+     {/* ------------------------------------ */}
+     {/*                  Form                */}
+     {/* ------------------------------------ */}
+        <form className="card-body">
+      {/* -----------------------------  email  --------------------------- */}
+          <div className="form-control ">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+         {/* -------------------------  password --------------------------- */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <h1>No account? <Link to="/register" className="font-bold ">Register</Link> </h1>
+         {/* ----------------------------  button  --------------------------- */}    
+          <div className="form-control mt-6">
+            <button className="btn btn-active btn-neutral">Login</button>
+          </div>
+        </form>
+      </div>
+      
+{/* ------------------------------  lottie animation -------------------------------- */}
+      <div className="flex-1">
+      <Lottie animationData={login} loop={true} />;
+      </div>
+
+     </div>
+
+
+    </div>
+  );
 };
 
 export default Login;
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -3,6 +3,7 @@ import Main from "../Components/Main/Main";
 import Login from "../Components/Pages.jsx/Login/Login";
 import Dashboard from './../Components/Pages.jsx/Dashboard/Dashboard';
 import Home from './../Components/Pages.jsx/Home/Home';
+import Register from "./../Components/Pages.jsx/Login/Register";
 
 const router = createBrowserRouter([
     {
@@ -21,6 +22,11 @@ const router = createBrowserRouter([
           path:"/dashboard",
           element:<Dashboard></Dashboard>
         },
+        {
+          path:"/register",
+          element:<Register></Register>
+        },
+        
       ]
       
     },
